8580e2c438376cf78c395aae205b2ca4b234afb7
HollysKim <hollyskim521@gmail.com>
2023-10-05 14:41:15 -0400

    first commit

---
+++ t.js
@@ -0,0 +1,6 @@
+const Hollys = [
+    {name: 'Hollys Kim'},
+    {age: 23},
+    {skills: ['HTML5', 'CSS', 'Javascript', 'React', 'Node.js', 'Git', 'Figma']},
+    {interests: ['front-end', 'manga', 'retro games', 'vintage t-shirts']}
+]
