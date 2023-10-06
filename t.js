a146e8d0bfb3ef3642edade29f09ad3c93d287ad
HollysKim <44379464+HollysKim@users.noreply.github.com>
2023-10-06 12:31:18 -0400

    Delete t.js

--- t.js
+++
@@ -1,6 +0,0 @@
-const Hollys = [
-    {name: 'Hollys Kim'},
-    {age: 23},
-    {skills: ['HTML5', 'CSS', 'Javascript', 'React', 'Node.js', 'Git', 'Figma']},
-    {interests: ['front-end', 'manga', 'retro games', 'vintage t-shirts']}
-]
